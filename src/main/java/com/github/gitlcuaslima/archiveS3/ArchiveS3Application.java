118c8526ca644edb67613e0264ca101ab29fff1a
Lucas Lima <57276011+gitlucaslima@users.noreply.github.com>
2023-12-20 10:54:02 -0500

    feature(upload, delete, get): Project of management archives in aws cloud

---
+++ src/main/java/com/github/gitlcuaslima/archiveS3/ArchiveS3Application.java
@@ -0,0 +1,13 @@
+package com.github.gitlcuaslima.archiveS3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiveS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchiveS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gitlcuaslima/archiveS3/ArchiveS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gitlcuaslima.archiveS3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiveS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
